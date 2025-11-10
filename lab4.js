46c29d48ae48703d1bad9dd8e8fc756e30478fe8
Ngô Văn Quyềnn <ngoquyen03092006@gmail.com>
2025-11-10 13:31:54 +0700

    lab4

---
+++ lab4.js
@@ -0,0 +1,60 @@
+function getFirstLast(array) {
+    // Your code here
+    return [array[0], array[array.length - 1]];
+}
+
+console.log(getFirstLast([1, 2, 3, 4])); // [1, 4]
+
+
+
+function swapElements(arr) {
+    // Your code here
+    const doiViTri = arr[1]
+    arr[1] = arr[3]
+    arr[3] = doiViTri
+    return arr
+}
+
+console.log(swapElements([1, 2, 3, 4, 5])); // [1, 4, 3, 2, 5]
+
+
+
+const user = {
+  id: 1,
+  personalInfo: {
+    name: "javascript",
+    contact: {
+      email: "javascript@email.com",
+      phone: "123-456-7890",
+    },
+  },
+};
+
+function getUserInfo(user) {
+  // Your code here
+  const userName = user.personalInfo.name
+  const userEmail = user.personalInfo.contact.email
+  return {
+    name: userName,
+    email: userEmail
+  }
+}
+
+console.log(getUserInfo(user));
+// { name: 'javascript', email: 'javascript@email.com' }
+
+
+
+function createProduct({ name, price, category = "general", inStock = true }) {
+  // Your code here
+  return{
+    name,
+    price,
+    category,
+    inStock
+  }
+}
+
+const product = createProduct({ name: "Laptop", price: 999 });
+console.log(product);
+// { name: 'Laptop', price: 999, category: 'general', inStock: true }

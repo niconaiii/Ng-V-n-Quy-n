73bc9563492b5548481f1dee12dca580e144179c
Ngô Văn Quyềnn <ngoquyen03092006@gmail.com>
2025-11-19 13:29:03 +0700

    Bai tap Props Component

--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -3,23 +3,23 @@ function Header() {
     <nav className="my-5 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <ul className="flex items-center justify-start space-x-10 text-2xl">
         <li>
-          <a className="font-bold text-blue-600" href="#">
-            Home
+          <a className="font-bold text-blue-600 hover:underline" href="#">
+            Trang Chủ
           </a>
         </li>
         <li>
-          <a className="font-semibold text-gray-700" href="#">
-            Tours
+          <a className="font-semibold text-gray-700 hover:underline" href="#">
+            Các Tours Du Lịch
           </a>
         </li>
         <li>
-          <a className="font-semibold text-gray-700" href="#">
-            Destinations
+          <a className="font-semibold text-gray-700 hover:underline" href="#">
+            Các Điểm Đến
           </a>
         </li>
         <li>
-          <a className="font-semibold text-gray-700" href="#">
-            Bookings
+          <a className="font-semibold text-gray-700 hover:underline" href="#">
+            Đặt Tour
           </a>
         </li>
       </ul>
